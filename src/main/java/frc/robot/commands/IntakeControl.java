864435730cd3b52ecf920c8829411664fc4ad354
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2024-02-08 16:38:53 -0500

    intake control fix

--- src/main/java/frc/robot/commands/IntakeControl.java
+++ src/main/java/frc/robot/commands/IntakeControl.java
@@ -3,19 +3,22 @@ package frc.robot.commands;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Intake;
 
 public class IntakeControl extends Command{
-
+    private Shooter s_Shooter;
     private Intake s_Intake;
     private DoubleSupplier lTrigger, rTrigger;
 
     public IntakeControl(Intake s_Intake, DoubleSupplier lTrigger, DoubleSupplier rTrigger) {
+//   public IntakeControl(Intake s_Intake, Shooter s_Shooter, DoubleSupplier lTrigger, DoubleSupplier rTrigger) {
         this.s_Intake = s_Intake;
+        // this.s_Shooter = s_Shooter;
         this.lTrigger = lTrigger;
         this.rTrigger = rTrigger;
 
-        addRequirements(s_Intake);
+        addRequirements(s_Intake, s_Shooter);
     }
     
     @Override
@@ -24,10 +27,12 @@ public class IntakeControl extends Command{
         double rValue = rTrigger.getAsDouble();
         if ((lValue > 0 && rValue > 0) || (lValue == 0 && rValue == 0)){
             s_Intake.stop();
+            // s_Shooter.stop();
             return;
         }
         double power = Math.max(lValue, rValue) * ( lValue > rValue ? -1 : 1) * -0.5;
         s_Intake.setPower(power);
+        s_Shooter.setPower(0.1);
     }
     
 }
