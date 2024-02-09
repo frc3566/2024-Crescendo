3ae11713b0a427d98c0045122c5278c59d375197
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2024-02-09 15:58:12 -0500

    updated intake control

--- src/main/java/frc/robot/commands/IntakeControl.java
+++ src/main/java/frc/robot/commands/IntakeControl.java
@@ -11,13 +11,12 @@ public class IntakeControl extends Command{
     private Intake s_Intake;
     private DoubleSupplier lTrigger, rTrigger;
 
-    public IntakeControl(Intake s_Intake, DoubleSupplier lTrigger, DoubleSupplier rTrigger) {
-//   public IntakeControl(Intake s_Intake, Shooter s_Shooter, DoubleSupplier lTrigger, DoubleSupplier rTrigger) {
+    // public IntakeControl(Intake s_Intake, DoubleSupplier lTrigger, DoubleSupplier rTrigger) {
+  public IntakeControl(Intake s_Intake, Shooter s_Shooter, DoubleSupplier lTrigger, DoubleSupplier rTrigger) {
         this.s_Intake = s_Intake;
-        // this.s_Shooter = s_Shooter;
+        this.s_Shooter = s_Shooter;
         this.lTrigger = lTrigger;
         this.rTrigger = rTrigger;
-
         addRequirements(s_Intake, s_Shooter);
     }
     
@@ -27,12 +26,44 @@ public class IntakeControl extends Command{
         double rValue = rTrigger.getAsDouble();
         if ((lValue > 0 && rValue > 0) || (lValue == 0 && rValue == 0)){
             s_Intake.stop();
-            // s_Shooter.stop();
+            s_Shooter.stop();
             return;
         }
         double power = Math.max(lValue, rValue) * ( lValue > rValue ? -1 : 1) * -0.5;
         s_Intake.setPower(power);
         s_Shooter.setPower(0.1);
+    }
+
+    public Shooter getS_Shooter() {
+        return s_Shooter;
+    }
+
+    public void setS_Shooter(Shooter s_Shooter) {
+        this.s_Shooter = s_Shooter;
+    }
+
+    public Intake getS_Intake() {
+        return s_Intake;
+    }
+
+    public void setS_Intake(Intake s_Intake) {
+        this.s_Intake = s_Intake;
+    }
+
+    public DoubleSupplier getlTrigger() {
+        return lTrigger;
+    }
+
+    public void setlTrigger(DoubleSupplier lTrigger) {
+        this.lTrigger = lTrigger;
+    }
+
+    public DoubleSupplier getrTrigger() {
+        return rTrigger;
+    }
+
+    public void setrTrigger(DoubleSupplier rTrigger) {
+        this.rTrigger = rTrigger;
     }
     
 }
