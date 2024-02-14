439306e4e08143bbfb5db89babd209fcdf2b6e53
GregoryLi360 <gregoryli360@gmail.com>
2024-02-14 16:28:03 -0500

    fix: move ctre config file

--- src/main/java/frc/lib/CTREConfigs.java
+++ src/main/java/frc/lib/CTREConfigs.java
@@ -1,10 +1,11 @@
-package frc.robot;
+package frc.lib;
 
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.MagnetSensorConfigs;
 import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
 // import com.ctre.phoenix.sensors.SensorTimeBase;
 
+import frc.robot.Constants;
 
 public final class CTREConfigs {
   public CANcoderConfiguration swerveCanCoderConfig;
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.lib.CTREConfigs;
 
 
 /**
