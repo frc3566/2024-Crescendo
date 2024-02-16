91020128d76fbf13d4343f0e9b73a3cfdd809ebb
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2024-02-16 16:58:33 -0500

    IntakeAndHold Done

--- src/main/java/frc/robot/commands/IntakeAndHold.java
+++ src/main/java/frc/robot/commands/IntakeAndHold.java
@@ -2,35 +2,56 @@ package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Intake;
 
 public class IntakeAndHold extends Command {
-    private Shooter s_Shooter;
     private Intake s_Intake;
+    private Timer s_Timer = new Timer();
 
     private final double intakeSpeed = 0.5;
-    private final double shooterReverseSpeed = -0.3;
-    private final double intakeReverseSpeed = -0.1;
+    private final double intakeReverseSpeed = -0.2;
+    private final double reverseTime = 2; //To be tested
+    private final double stopTime = 2.5; //To be tested
+    private boolean isDone = false;
 
-    public IntakeAndHold(Intake s_Intake, Shooter s_Shooter) {
+    public IntakeAndHold(Intake s_Intake) {
         this.s_Intake = s_Intake;
-        this.s_Shooter = s_Shooter;
-        addRequirements(s_Intake, s_Shooter);
+        addRequirements(s_Intake);
     }
     
     @Override
+    public void initialize() {
+        s_Timer.reset();
+        s_Timer.start();
+    }
+
+    @Override
     public void execute() {
-        s_Intake.setPower(intakeSpeed);
-        s_Shooter.setPower(shooterReverseSpeed);
+
+        if(s_Timer.get()>=stopTime){
+            isDone = true;
+            end(false);
+        }
+
+        else if(s_Timer.get() >= reverseTime) {
+            s_Intake.setPower(intakeReverseSpeed);
+        }
+
+        else{
+            s_Intake.setPower(intakeSpeed);
+        }
 
     }
 
     @Override
     public void end(boolean interrupted) {
         s_Intake.stop();
-        s_Shooter.stop();
-        
+    }
+
+    public boolean isFinished() {
+        return isDone;
     }
 }
