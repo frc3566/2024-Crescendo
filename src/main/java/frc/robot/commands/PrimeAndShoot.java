ce45223c6eac8d68e9423b478335838234792fba
CooperWang0912 <cooperwang0912@gmail.com>
2024-02-16 16:17:23 -0500

    Prime and Shoot functionality complete

--- src/main/java/frc/robot/commands/PrimeAndShoot.java
+++ src/main/java/frc/robot/commands/PrimeAndShoot.java
@@ -3,11 +3,15 @@ package frc.robot.commands;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj.Timer;
 
 public class PrimeAndShoot extends Command {
     private Shooter s_Shooter;
+    private Intake s_Intake;
     private final double targetSpeed;
+    private Timer timer = new Timer();
 
     public PrimeAndShoot(Shooter s_Shooter, double targetSpeed) {
         this.s_Shooter = s_Shooter;
@@ -17,6 +21,10 @@ public class PrimeAndShoot extends Command {
     
     public void execute() {
         s_Shooter.setPower(targetSpeed);
+        timer.start();
+        if (timer.get() >= 3){
+            s_Intake.setPower(0.7);
+        }
 
         /* TODO: run intake when shooter is at target speed */
     }
