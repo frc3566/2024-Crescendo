9c21101d51ff05363872175bcb5161d41d94cd1d
GregoryLi360 <gregoryli360@gmail.com>
2024-02-16 23:35:28 -0500

    feat: update to IntakeAndHold

--- src/main/java/frc/robot/autos/TwoPiece.java
+++ src/main/java/frc/robot/autos/TwoPiece.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.intake.IntakeAndHold;
 import frc.robot.commands.intake.IntakeTest;
 import frc.robot.commands.shooter.PrimeAndShoot;
 import frc.robot.commands.shooter.Shoot;
@@ -17,9 +18,8 @@ public class TwoPiece extends SequentialCommandGroup {
     public TwoPiece(Swerve s_Swerve, Intake s_Intake, Shooter s_Shooter) {
         Command driveToFirstPosition = new Drive(s_Swerve, new Pose2d());
         Command shoot = new PrimeAndShoot(s_Shooter, 0.7);
-        Command driveAndIntake = new DriveAndIntake(s_Swerve, s_Intake, new Pose2d());
+        Command driveAndIntake = new DriveAndIntake(s_Swerve, s_Intake, s_Shooter, new Pose2d());
         
-
         addCommands(
             driveToFirstPosition,
             shoot,
@@ -29,9 +29,9 @@ public class TwoPiece extends SequentialCommandGroup {
 }
 
 class DriveAndIntake extends SequentialCommandGroup {
-    public DriveAndIntake(Swerve s_Swerve, Intake s_Intake, Pose2d pose) {
+    public DriveAndIntake(Swerve s_Swerve, Intake s_Intake, Shooter s_Shooter, Pose2d pose) {
         Command drive = new Drive(s_Swerve, pose);
-        Command intake = new IntakeTest(s_Intake, () -> 0.7);
+        Command intake = new IntakeAndHold(s_Intake, s_Shooter);
 
         Command driveAndIntake = new ParallelDeadlineGroup(drive, intake);
 
