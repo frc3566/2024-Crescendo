d82b2cd6f5b2768e429e144e2174e6edc32cc165
zerrrr0 <tianhezhang@stmarksschool.org>
2024-02-24 13:46:01 -0500

    skew test

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
+import edu.wpi.first.math.geometry.Rotation2d;
 
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
@@ -9,16 +10,20 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.Timer;
 
-
-public class TeleopSwerve extends Command {    
-    private Swerve s_Swerve;    
+public class TeleopSwerve extends Command {
+    private Swerve s_Swerve;
     private DoubleSupplier translationSup;
     private DoubleSupplier strafeSup;
     private DoubleSupplier rotationSup;
     private BooleanSupplier robotCentricSup;
+    private double dt, prevTime;
+    private Rotation2d offset;
+    private Timer timer;
 
-    public TeleopSwerve(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
+    public TeleopSwerve(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup,
+            DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
         this.s_Swerve = s_Swerve;
         addRequirements(s_Swerve);
 
@@ -29,18 +34,33 @@ public class TeleopSwerve extends Command {
     }
 
     @Override
+    public void initialize() {
+        timer.restart();
+        prevTime = 0.0;
+        dt = 0.0;
+    }
+
     public void execute() {
+        // timer
+        double currTime = timer.get();
+        dt = currTime - prevTime;
+        prevTime = currTime;
         /* Get Values, Deadband */
+        // x
         double translationVal = MathUtil.applyDeadband(translationSup.getAsDouble(), Constants.Swerve.stickDeadband);
+        // y
         double strafeVal = MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.Swerve.stickDeadband);
+        //rotation
         double rotationVal = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.Swerve.stickDeadband);
+        offset = new Rotation2d(-rotationVal * Constants.Swerve.maxAngularVelocity * dt);
+        // Skew
+        Translation2d test = new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed).rotateBy(offset);
 
         /* Drive */
         s_Swerve.drive(
-            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed), 
-            rotationVal * Constants.Swerve.maxAngularVelocity, 
-            !robotCentricSup.getAsBoolean(), 
-            true
-        );
+                test,
+                rotationVal * Constants.Swerve.maxAngularVelocity,
+                !robotCentricSup.getAsBoolean(),
+                true);
     }
 }
