120b3439d03df78d161f0f3f4c99e3ce986b1ea1
CooperWang0912 <cooperwang0912@gmail.com>
2024-02-24 13:23:50 -0500

    Intake Autonomous Code Timer Addition

--- src/main/java/frc/robot/commands/TakeIn.java
+++ src/main/java/frc/robot/commands/TakeIn.java
@@ -3,11 +3,13 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj.Timer;
 
 public class TakeIn extends Command{
 
     Shooter s_Shooter;
     Intake s_Intake;
+    Timer timer = new Timer();
 
     public final double intakeSpeed = 0.5;
     public final double shooterReverseSpeed = -0.3;
@@ -15,6 +17,11 @@ public class TakeIn extends Command{
     public TakeIn(Shooter s_Shooter, Intake s_Intake){
         this.s_Shooter = s_Shooter;
         this.s_Intake = s_Intake;
+    }
+
+    public void initialize(){
+        timer.reset();
+        timer.start();
     }
 
     @Override
@@ -30,8 +37,7 @@ public class TakeIn extends Command{
     }
 
     public boolean isFinished(){
-        //TODO: Get sensors to detect the ring and return boolean
-        return false;
+        return timer.get() >= 3;
     }
     
 }
