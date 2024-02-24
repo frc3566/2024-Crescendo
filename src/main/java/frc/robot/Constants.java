2d13bfb6c2311e6e0ed0147462783eee663af1de
kaaaien <kaienhung999@gmail.com>
2024-02-24 14:03:00 -0500

    feat: new wheelbase+trackwidth

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,8 +33,8 @@ public final class Constants {
     public static final boolean invertGyro = true; // Always ensure Gyro is CCW+ CW-
 
     /* Drivetrain Constants */
-    public static final double trackWidth = Units.inchesToMeters(27.625);
-    public static final double wheelBase = Units.inchesToMeters(27.625);
+    public static final double trackWidth = Units.inchesToMeters(24.25);
+    public static final double wheelBase = Units.inchesToMeters(24.25);
     public static final double wheelDiameter = Units.inchesToMeters(4.0);
     public static final double wheelCircumference = wheelDiameter * Math.PI;
 
